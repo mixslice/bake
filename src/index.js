5868994eb8c4f075662d6f2500238706fa2b1a40
Zeqing Zhang <zzq889@gmail.com>
2016-09-09 18:12:40 +0800

    add comments for example

--- src/index.js
+++ src/index.js
@@ -3,29 +3,18 @@ import concat from './concat';
 
 
 // sample 1
-const data1 = [
-  { x: 1, y: 3 },
-  { x: 5, y: 8 },
-  { x: 10, y: 12 },
-  { x: 15, y: 19 },
-];
-const data2 = { x: 4, y: 11 };
-
-console.log('data1', data1);
-console.log('data2', data2);
-console.log('subtract', subtract(data1, data2));
-
-// sample 2
-const data3 = [
+const rendered = [
   { x: 1, y: 3, data: '[1:3]' },
   { x: 5, y: 8, data: '[5:8]' },
   { x: 10, y: 12, data: '[10:12]' },
   { x: 15, y: 19, data: '[15:19]' },
 ];
-const data4 = [
-  { x: 4, y: 11, data: '[4:11]' },
-];
+const renderRange = { x: 4, y: 11 };
 
-console.log('data3', data3);
-console.log('data4', data4);
-console.log('concat', concat(data3, data4));
+console.log('rendered cache for block', rendered);
+console.log('render range for block', renderRange);
+const needToRender = subtract(rendered, renderRange);
+console.log('need to render', needToRender);
+const newRendered = needToRender.map(({ x, y }) => ({ x, y, data: `[${x}:${y}]` }));
+console.log('new rendered', newRendered);
+console.log('save new rendered to block cache', concat(rendered, newRendered));
