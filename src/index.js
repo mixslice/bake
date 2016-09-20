2b26e603ca839c5d311e2f86fbd2b282409a1bdc
Zeqing Zhang <zzq889@gmail.com>
2016-09-21 00:46:16 +0800

    add babel debug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,37 @@ import {
   readFilePromise,
 } from './utils';
 
+const mapReduce = (arr) => (
+  arr
+  .map(d => ({ [d.$.id]: d.$ }))
+  .reduce((result, item) => {
+    const newResult = result;
+    const key = Object.keys(item)[0];
+    newResult[key] = item[key];
+    return newResult;
+  })
+);
+
+const processData = ({
+  fcpxml: {
+    resources,
+    library,
+  },
+}) => {
+  const { format, asset, effect } = resources[0];
+  const formats = mapReduce(format);
+  const assets = mapReduce(asset);
+  const effects = mapReduce(effect);
+
+  const project = library[0].event[0].project[0];
+  const spine = project.sequence[0].spine[0].clip;
+
+  console.log(spine);
+};
+
 const filename = path.join(__dirname, 'sample/test.xml');
 
 readFilePromise(filename)
 .then(data => parserPromise(data))
-.then(result => console.dir(result))
+.then(processData)
 .catch(e => console.err(e));
